4c51631b97e8f6464b3822618096694be7d1dc64
brunonasc74 <bruno.hpn74@gmail.com>
2023-03-27 11:57:03 -0300

    feat: Initial config create

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './theme';
+import useDarkMode from './hooks/useDarkMode';
+import GlobalStyles from './components/GlobalStyles';
+
 function App() {
-  return <div>Hello React</div>;
+  const [theme, themeToggler] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+  return (
+    <ThemeProvider theme={themeMode}>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route path='/' element={<h1>Hello React</h1>} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
